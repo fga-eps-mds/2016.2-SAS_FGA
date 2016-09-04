e4792a7f8da8bf4ffcf03ff4daa48befbce9de4f
Gustavo Coelho <gutorc@hotmail.com>
2016-09-03 21:06:23 -0300

    Fixing register form
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- sas/booking/forms.py
+++ sas/booking/forms.py
@@ -1,20 +1,29 @@
 from django.forms import ModelForm
 from .models import UserProfile
 from django import forms
-
+from django.contrib.auth.models import User
 class UserForm(ModelForm):
   name = forms.CharField(label = 'Nome:')
+  username = forms.CharField(label = 'Usurário:')
   email = forms.CharField(label = 'Email:')
   password = forms.CharField(label = 'Senha:', widget = forms.PasswordInput())
   repeat_password = forms.CharField(label = 'Repetir Senha:', widget = forms.PasswordInput())
 
-  def save(self):
+  def save(self, force_insert=False, force_update=False, commit=True):
+    userprofile = super(UserForm, self).save(commit=False)
     user = User()
-    user.name = self.name
-    user.email = self.email
-    user.set_password(self.password)
+    user.name = self.cleaned_data.get('name')
+    user.email = self.cleaned_data.get('email')
+    user.username = self.cleaned_data.get('username')
+    user.set_password(self.cleaned_data.get('password'))
     user.save()
-    super(UserForm,self).save()
+
+    userprofile.user = user
+    # do custom stuff
+    if commit:
+        userprofile.save()
+    return userprofile
+
   def clean(self):
     cleaned_data = super(UserForm,self).clean()
     if cleaned_data.get('password') != cleaned_data.get('repeat_password'):
--- sas/booking/views.py
+++ sas/booking/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import UserForm
 from .models import UserProfile
+
 def new_user(request):
   if request.method == "POST":
-    form = UserForm(request)
+    form = UserForm(request.POST,UserProfile)
     if not(form.is_valid()):
       return render(request, 'booking/newUser.html', {'form_user':form})
     else:
       user_profile = form.save()
+      return redirect("/user/newuser/",{'confirmation': True})
   else:
     form = UserForm()
     return render(request, 'booking/newUser.html', {'form_user':form})
