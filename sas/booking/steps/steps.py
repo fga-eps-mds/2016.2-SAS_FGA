3886e76228eb0169c01580a1a8a0cbd86eb1269c
lucas <lucas@localhost.localdomain>
2016-09-08 18:09:26 -0300

    Created a new step

--- sas/booking/steps/steps.py
+++ sas/booking/steps/steps.py
@@ -1,6 +1,7 @@
 from aloe import step,world
 from aloe_webdriver.util import find_field_by_id,find_any_field
 from aloe_webdriver import TEXT_FIELDS
+from booking.models import UserProfile
 
 @step(r'I type in "(.*)" to "(.*)"')
 def fill_bootstrap_field(step,text,field):
@@ -9,3 +10,13 @@ def fill_bootstrap_field(step,text,field):
   id_field = "_".join(words_list)
   date_field = find_any_field(world.browser,TEXT_FIELDS,id_field)
   date_field.send_keys(text)
+
+@step(r'I register the user "(.*)" with the password "(.*)"')
+def fill_bootstrap_field(step,username,password):
+  user = UserProfile()
+  user.registration_number = "23412"
+  user.user.email = "sdsade@dasd.com"
+  user.user.first_name = "Pudim"
+  user.user.username = username
+  user.user.set_password(password)
+  user.save()
