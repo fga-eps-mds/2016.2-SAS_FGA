5c503a135139e5b6f2b1068690b7a0b94100243b
fabiolamfleury <fabiola.mfleury@gmail.com>
2016-09-11 19:23:28 -0300

    Added unit test for delete user case

--- sas/booking/tests.py
+++ sas/booking/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .views import delete_user
+from .models import UserProfile
 
-# Create your tests here.
+class DeleteUserTestCase(TestCase):
+
+    user_profile = UserProfile()
+    user_profile.name = 'Teste'
+    user_profile.category = 'Aluno'
+    user_profile.registration_number = 10;
+    user_profile.email = 'example@example.com'
+    id_test = user_profile.id
+
+    def setUp(self):
+        self.delete_user = delete_user()
+    def delete_confirmed(self):
+        self.assertEqual(delete_user(request.POST['delete'],id_test), render(request, 'booking/index.html',{}))
+    def delete_cancelled(self):
+        self.assertEqual(delete_user(request.POST['cancel'],id_test), render(request,'booking/listUser.html',user_profile))
