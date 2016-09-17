5ceace1b3ad8be1199f1b79051f1dceb9a34d14b
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2016-09-17 12:34:10 -0300

    Adding delete functional test

--- sas/booking/features/delete.feature
+++ sas/booking/features/delete.feature
@@ -1,8 +1,18 @@
 Feature: Delete
 
+Background:
+  Given I register the user "lucas@email.com" with the password "123456"
+  When I visit site page "/"
+  Then I should see an element with id of "enter-button"
+  Then I click on an element with id of "enter-button"
+  And I fill in "Email" with "lucas@email.com"
+  And I fill in "Password" with "123456"
+  Then I press "Entrar"
+
 Scenario: User already registered and wants to delete their account
-  When I visit site page "/user/listuser"
-  And I press "Deletar conta"
+  Then I should see an element with id of "user-link"
+  Then I click on an element with id of "user-link"
+  Then I should see "Meus Dados" on an element "li"
+  And I click on an element "li" called "Meus Dados"
+  And I press "Excluir minha conta"
   Then I am redirected to "/user/deleteUser"
-  Then I press "Deletar"
-  Then I should see "Usuário deletado com sucesso!"
--- sas/booking/steps/steps.py
+++ sas/booking/steps/steps.py
@@ -1,6 +1,7 @@
 from aloe import step, world
 from aloe_webdriver.util import find_field_by_id, find_any_field, find_field_by_value
 from aloe_webdriver import TEXT_FIELDS
+from selenium.common.exceptions import NoSuchElementException
 from booking.models import UserProfile
 from django.contrib.auth.models import User
 
@@ -22,13 +23,20 @@ def click_on_element_by_id(step, id):
 		raise AssertionError("Element with ID '{}' not found.".format(id))
 	elem.click()
 
-@step(r'see "(.*)" on an element with id of "(.*)"')
-def find_element_value(step, elementtext, id):
+@step(r'I click on an element "(.*)" called "(.*)"')
+def click_on_element_by_value(step, value, typeelement):
 	try:
-		text = find_field_by_value(world.browser, 'a', elementtext)
+		text = find_field_by_value(world.browser, typeelement, elementtext)
 	except NoSuchElementException:
 		raise AssertionError("Element not found.")
+	text.click()
 
+@step(r'I see "(.*)" on an element "(.*)"')
+def find_element_value(step, elementtext, typeelement):
+	try:
+		text = find_field_by_value(world.browser, typeelement, elementtext)
+	except NoSuchElementException:
+		raise AssertionError("Element not found.")
 
 @step(r'I register the user "(.*)" with the password "(.*)"')
 def register_user(step, username, password):
@@ -39,3 +47,11 @@ def register_user(step, username, password):
 	user.user.first_name = "Usuário"
 	user.user.set_password(password)
 	user.save()
+
+@step(r'I log in the user "(.*)" with the password "(.*)"')
+def user_login(step, username, password):
+	click_on_element_by_id(step, 'enter-button')
+	fill_bootstrap_field(step, username, 'Username')
+	fill_bootstrap_field(step, password, 'Password')
+	click_on_element_by_id(step, 'btn-login')
+
