ca8642bb88cb5af66d4609a51ea56de298a8a086
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2016-09-16 23:31:20 -0300

    Fixing and adding functional tests regarding login

--- sas/booking/features/register.feature
+++ sas/booking/features/register.feature
@@ -2,13 +2,11 @@ Feature: Register
 
 Scenario: User not registered
   When I visit site page "/user/newuser"
-  And I type in "12/0030559" to "Registration Number"
+  And I type in "120030559" to "Registration Number"
   And I select "Student" from "Category"
   And I fill in "Name" with "Pedro Pereira Pinto"
   And I fill in "Email" with "pedropp@gmail.com"
   And I type in "teste123" to "Password"
   And I type in "teste123" to "Repeat Password"
   Then I press "Cadastrar"
-  Then I should see "Welcome to SAS"
-
-
+  Then I should see page "/"
--- sas/booking/steps/steps.py
+++ sas/booking/steps/steps.py
@@ -1,5 +1,5 @@
 from aloe import step, world
-from aloe_webdriver.util import find_field_by_id, find_any_field
+from aloe_webdriver.util import find_field_by_id, find_any_field, find_field_by_value
 from aloe_webdriver import TEXT_FIELDS
 from booking.models import UserProfile
 from django.contrib.auth.models import User
@@ -22,14 +22,20 @@ def click_on_element_by_id(step, id):
 		raise AssertionError("Element with ID '{}' not found.".format(id))
 	elem.click()
 
+@step(r'see "(.*)" on an element with id of "(.*)"')
+def find_element_value(step, elementtext, id):
+	try:
+		text = find_field_by_value(world.browser, 'a', elementtext)
+	except NoSuchElementException:
+		raise AssertionError("Element not found.")
+
 
 @step(r'I register the user "(.*)" with the password "(.*)"')
 def register_user(step, username, password):
 	user = UserProfile()
 	user.user = User()
-	user.registration_number = "23412"
+	user.registration_number = "140016574"
 	user.user.email = username
-	user.user.first_name = "Pudim"
-	user.user.username = username
+	user.user.first_name = "Usuário"
 	user.user.set_password(password)
 	user.save()
