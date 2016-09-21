f5e73ce39275f50f89eeab381914b72aee71c0b9
Jessica Suzuki <suzuki_jessica@hotmail.com>
2016-09-21 00:56:30 -0300

    Changing tests.py according to pep8

--- sas/booking/tests.py
+++ sas/booking/tests.py
@@ -3,6 +3,8 @@ from booking.models import *
 from django.test import Client
 from booking.factories import *
 from datetime import datetime
+
+
 class ViewsTest(TestCase):
 
 	def setUp(self):
@@ -10,65 +12,68 @@ class ViewsTest(TestCase):
 
 	def test_index(self):
 		result = self.client.get("/")
-		self.assertEqual(result.status_code,200)
+		self.assertEqual(result.status_code, 200)
 
 	def test_new_user(self):
 		result = self.client.get("/user/newuser/")
-		self.assertEqual(result.status_code,200)
+		self.assertEqual(result.status_code, 200)
+
 
 class UserProfileTest(TestCase):
-	
+
 	def setUp(self):
 		self.userprofile = UserProfile()
 
 	def test_set_name(self):
 		self.userprofile.name("Gustavo Rodrigues Coelho")
-		self.assertEqual(self.userprofile.user.first_name,"Gustavo")
-		self.assertEqual(self.userprofile.user.last_name,"Rodrigues Coelho")
+		self.assertEqual(self.userprofile.user.first_name, "Gustavo")
+		self.assertEqual(self.userprofile.user.last_name, "Rodrigues Coelho")
 
 	def test_get_full_name(self):
 		name = "Pedro Pereira Pinto"
 		self.userprofile.name(name)
-		self.assertEqual(self.userprofile.full_name(),name)
+		self.assertEqual(self.userprofile.full_name(), name)
 
 	def test_category(self):
-		self.assertEqual(len(CATEGORY),3)		
-	
+		self.assertEqual(len(CATEGORY), 3)
+
 	def test_save(self):
 		self.userprofile.name("Gustavo Rodrigues Coelho")
 		self.userprofile.registration_number = "11/0030559"
 		self.userprofile.user.username = "gutorc@hotmail.com"
 		self.userprofile.user.email = "gutorc@hotmail.com"
 		self.userprofile.save()
-		self.assertEqual(self.userprofile.pk,1)
+		self.assertEqual(self.userprofile.pk, 1)
+
 
 class TestBookTime(TestCase):
 
 	def test_add_days(self):
 		book = BookTime()
-		book.date_booking = datetime.strptime("01022010","%d%m%Y")
+		book.date_booking = datetime.strptime("01022010", "%d%m%Y")
 		book.add_days(5)
-		self.assertEqual(book.date_booking.strftime("%d%m%Y"),"06022010")
-	
+		self.assertEqual(book.date_booking.strftime("%d%m%Y"), "06022010")
+
 	def test_next_week_day(self):
 		book = BookTime()
-		book.date_booking = datetime.strptime("21092016","%d%m%Y")
+		book.date_booking = datetime.strptime("21092016", "%d%m%Y")
 		book.next_week_day(4)
-		self.assertEqual(book.date_booking.strftime("%d%m%Y"),"23092016")
-		book.date_booking = datetime.strptime("21092016","%d%m%Y")
+		self.assertEqual(book.date_booking.strftime("%d%m%Y"), "23092016")
+		book.date_booking = datetime.strptime("21092016", "%d%m%Y")
 		book.next_week_day(2)
-		self.assertEqual(book.date_booking.strftime("%d%m%Y"),"28092016")
-		book.date_booking = datetime.strptime("20092016","%d%m%Y")
+		self.assertEqual(book.date_booking.strftime("%d%m%Y"), "28092016")
+		book.date_booking = datetime.strptime("20092016", "%d%m%Y")
 		book.next_week_day(0)
-		self.assertEqual(book.date_booking.strftime("%d%m%Y"),"26092016")
+		self.assertEqual(book.date_booking.strftime("%d%m%Y"), "26092016")
 
 	def test_get_str_weekday(self):
 		book = BookTime()
-		book.date_booking = datetime.strptime("21092016","%d%m%Y")
-		self.assertEqual(book.get_str_weekday(),"Wednesday")
+		book.date_booking = datetime.strptime("21092016", "%d%m%Y")
+		self.assertEqual(book.get_str_weekday(), "Wednesday")
+
 
 class TestBooking(TestCase):
-	
+
 	def setUp(self):
 		self.booking = Booking()
 
@@ -79,4 +84,4 @@ class TestBooking(TestCase):
 		self.booking.end_date = datetime.now()
 		self.booking.place = PlaceFactory.create()
 		self.booking.save()
-		self.assertEqual(self.booking.pk,1)
+		self.assertEqual(self.booking.pk, 1)
