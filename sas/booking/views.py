412551bae27fe5ca00df7ae7fbfa2af7063d44cf
Jessica Suzuki <suzuki_jessica@hotmail.com>
2016-09-21 01:26:02 -0300

    Changing views.py according to pep8

--- sas/booking/views.py
+++ sas/booking/views.py
@@ -10,7 +10,8 @@ from django.contrib import messages
 
 def index(request):
     form = LoginForm()
-    return render(request, 'booking/index.html', {'form':form})
+    return render(request, 'booking/index.html', {'form': form})
+
 
 def new_user(request):
 	if request.method == "POST":
@@ -36,14 +37,15 @@ def edit_user(request):
 		form = EditUserForm(request.POST, instance=request.user.profile_user)
 		if form.is_valid():
 			user = form.save()
-			messages.success(request,_('Your data has been updated'))
-		return render_edit_user(request,user_form=form)
+			messages.success(request, _('Your data has been updated'))
+		return render_edit_user(request, user_form=form)
 	elif not request.user.is_authenticated():
 		return index(request)
 	else:
 		return render_edit_user(request)
 
-def render_edit_user(request,user_form=None,change_form=PasswordForm()):
+
+def render_edit_user(request, user_form=None, change_form=PasswordForm()):
 	user = request.user
 	initial = {}
 	initial['name'] = user.profile_user.full_name()
@@ -53,23 +55,24 @@ def render_edit_user(request,user_form=None,change_form=PasswordForm()):
 								 instance=request.user.profile_user)
 	return render(request,
 				  'booking/editUser.html',
-				  {'form_user': user_form,'change_form':change_form})
+				  {'form_user': user_form, 'change_form': change_form})
 
 
-def login_user(request) :
+def login_user(request):
 	if request.method == "POST":
 		form = LoginForm(request.POST)
 		if form.is_valid():
 			user = form.save()
 			if user is not None:
-				login ( request , user) ;
-				return render (request ,'booking/home.html',{})
+				login(request, user);
+				return render(request, 'booking/home.html', {})
 			else:
-				return render (request,'booking/index.html',{'form':form})
+				return render(request, 'booking/index.html', {'form': form})
 		else:
-			return render (request,'booking/index.html',{'form':form})
+			return render(request, 'booking/index.html', {'form': form})
 	else:
 		return index(request)
+
 
 def logout_user(request):
     logout(request)
@@ -85,16 +88,17 @@ def delete_user(request):
 	else:
 		return index(request)
 
+
 def change_password(request):
 	if request.user.is_authenticated() and request.POST:
 		form = PasswordForm(request.POST)
 		if form.is_valid() and form.is_password_valid(request.user.username):
 			form.save(request.user)
-			login(request,request.user)
-			messages.success(request,_('Your password has been changed'))
+			login(request, request.user)
+			messages.success(request, _('Your password has been changed'))
 			return render_edit_user(request)
 		else:
-			return render_edit_user(request,change_form=form)
+			return render_edit_user(request, change_form=form)
 	if not request.user.is_authenticated():
 		return index(request)
 	else:
@@ -106,53 +110,55 @@ def new_booking(request):
 		if request.method == "POST":
 			form_booking = BookingForm(request.POST)
 			if not(form_booking.is_valid()):
-				return render(request, 'booking/newBooking.html', {'form_booking':form_booking})
+				return render(request, 'booking/newBooking.html', {'form_booking': form_booking})
 			else:
 				booking = form_booking.save(request.user)
 				if not booking:
-					messages.error(request,_("Booking alread exists"))
-					return render(request, 'booking/newBooking.html', {'form_booking':form_booking})
+					messages.error(request, _("Booking alread exists"))
+					return render(request, 'booking/newBooking.html', {'form_booking': form_booking})
 				request.session['booking'] = booking.pk
-				return render(request, 'booking/showDates.html', {'booking':booking})
+				return render(request, 'booking/showDates.html', {'booking': booking})
 		else:
 			form_booking = BookingForm()
-			return render(request, 'booking/newBooking.html', {'form_booking':form_booking})
+			return render(request, 'booking/newBooking.html', {'form_booking': form_booking})
 
 	else:
-		return index(request) 
+		return index(request)
+
 
 def search_booking(request):
     if request.user.is_authenticated():
         bookings = Booking.objects.filter(user=request.user)
-        return render(request, 'booking/searchBooking.html', {'bookings':bookings})
+        return render(request, 'booking/searchBooking.html', {'bookings': bookings})
     else:
         form = LoginForm()
-        return render(request, 'booking/index.html', {'form':form})
+        return render(request, 'booking/index.html', {'form': form})
 
-def cancel_booking(request,id):
+
+def cancel_booking(request, id):
 	if request.user.is_authenticated() and request.session['booking']:
 		id = int(id)
 		if(id == request.session.get('booking')):
 			request.session.pop('booking')
 			Booking.objects.get(pk=id).delete()
-			messages.success(request,_("Booking has been canceled"))
+			messages.success(request, _("Booking has been canceled"))
 			return index(request)
 		else:
-			messages.error(request,_("You cannot cancel this booking"))
+			messages.error(request, _("You cannot cancel this booking"))
 			return index(request)
 	else:
 		return index(request)
 
-def confirm_booking(request,id):
+
+def confirm_booking(request, id):
 	if request.user.is_authenticated() and request.session['booking']:
 		id = int(id)
 		if id == request.session.get('booking'):
 			request.session.pop('booking')
-			messages.success(request,_("Booking has been saved."))
+			messages.success(request, _("Booking has been saved."))
 			return index(request)
 		else:
-			messages.error(request,_("You cannot confirm this booking"))
+			messages.error(request, _("You cannot confirm this booking"))
 			return index(request)
 	else:
 		return index(request)
-
