78a0e3a3ac6e33df3ecc596454b321109991b42d
Vitor Menezes <menezesvitorg@gmail.com>
2016-10-14 20:11:42 -0300

    Created delete booking test.
    Signed-off-by: Vitor Menezes <menezesvitorg@gmail.com>

--- sas/booking/tests.py
+++ sas/booking/tests.py
@@ -4,6 +4,8 @@ from django.test import Client
 from booking.factories import *
 from datetime import datetime
 from user.factories import UserFactory
+from booking.factories import BookingFactory, BookTimeFactory
+from django.urls import reverse
 
 class TestBookTime(TestCase):
 
@@ -11,3 +13,21 @@ class TestBookTime(TestCase):
 		book = BookTime()
 		book.date_booking = datetime.strptime("21092016", "%d%m%Y")
 		self.assertEqual(book.get_str_weekday(), "Wednesday")
+
+class BookingTest(TestCase):
+
+	def setUp(self):
+		booktimes = [BookTimeFactory.create() for count in range(5)]
+		booking = BookingFactory.create(time = booktimes)
+		self.user = UserFactory.create()
+		self.user.set_password('1234567')
+		self.user.save()
+		booking.user = self.user
+		self.client = Client()
+		self.user = book.user
+
+	def has_permission_delete_booking(self):
+		self.client.login(username = self.user.username, password = '1234567')
+		url = reverse('booking:deletebooking', args = (booking.id,))
+		response = self.client.get('booking/deletebooking')
+		self.assertContains(response, 'Booking deleted!')
