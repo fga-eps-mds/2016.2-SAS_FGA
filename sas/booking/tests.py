a02b73889f2c3b0e10df62cd28e4b9f55fdb0861
Allan <pereirasallan@gmail.com>
2016-10-17 00:06:58 -0200

    Removed old BookTime tests

--- sas/booking/tests.py
+++ sas/booking/tests.py
@@ -7,24 +7,6 @@ from user.factories import UserFactory
 
 class TestBookTime(TestCase):
 
-	def test_add_days(self):
-		book = BookTime()
-		book.date_booking = datetime.strptime("01022010", "%d%m%Y")
-		book.add_days(5)
-		self.assertEqual(book.date_booking.strftime("%d%m%Y"), "06022010")
-
-	def test_next_week_day(self):
-		book = BookTime()
-		book.date_booking = datetime.strptime("21092016", "%d%m%Y")
-		book.next_week_day(4)
-		self.assertEqual(book.date_booking.strftime("%d%m%Y"), "23092016")
-		book.date_booking = datetime.strptime("21092016", "%d%m%Y")
-		book.next_week_day(2)
-		self.assertEqual(book.date_booking.strftime("%d%m%Y"), "28092016")
-		book.date_booking = datetime.strptime("20092016", "%d%m%Y")
-		book.next_week_day(0)
-		self.assertEqual(book.date_booking.strftime("%d%m%Y"), "26092016")
-
 	def test_get_str_weekday(self):
 		book = BookTime()
 		book.date_booking = datetime.strptime("21092016", "%d%m%Y")
