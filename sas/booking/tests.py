9d26d6e60c0500f6ed2f2ad08a38e87fa74fcf54
Vitor Menezes <menezesvitorg@gmail.com>
2016-10-19 01:04:37 -0200

    Fixing conflicts in booking factories.
    Signed-off-by: Vitor Menezes <menezesvitorg@gmail.com>

--- sas/booking/tests.py
+++ sas/booking/tests.py
@@ -14,7 +14,7 @@ class TestBookTime(TestCase):
 	def test_get_str_weekday(self):
 		book = BookTime()
 		book.date_booking = datetime.strptime("21092016", "%d%m%Y")
-		self.assertEqual(book.get_str_weekday(), "Wednesday")
+		self.assertEqual(book.get_str_weekday(), "Wednesday") 
 
 class BookingTest(TestCase):
 
