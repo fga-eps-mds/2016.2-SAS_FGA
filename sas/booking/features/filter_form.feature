9617e61a82b62afa65ee1b25f615ca5ba3d2d8c7
fabiolamfleury <fabiola.mfleury@gmail.com>
2016-10-18 22:51:43 -0200

    Creating acceptance tests for the filtering form
    Signed-off-by: fabiolamfleury <fabiola.mfleury@gmail.com>
    Signed-off-by: Elaine Cristina Meirelles <ecmeirelles@hotmail.com>

---
+++ sas/booking/features/filter_form.feature
@@ -0,0 +1,28 @@
+Feature: Search Bookings
+
+Background:
+  Given I register the user "lucas@gmail.com" with the password "123456" and registration number "140016574"
+
+Scenario: Filtering by Day x Room
+  When I login in with email "lucas@gmail.com" and password "123456"
+  Then I visit site page "/booking/searchbookingg/"
+  Then I choose "Day x Room"
+  Then There should be exactly 2 elements matching $(":disabled")
+
+Scenario: Filtering by Booking x Week
+  When I login in with email "lucas@gmail.com" and password "123456"
+  Then I visit site page "/booking/searchbookingg/"
+  Then I choose "Booking x Week"
+  Then There should be exactly 3 elements matching $(":disabled")
+
+Scenario: Filtering by Building x Day
+  When I login in with email "lucas@gmail.com" and password "123456"
+  Then I visit site page "/booking/searchbookingg/"
+  Then I choose "Building x Day"
+  Then There should be exactly 3 elements matching $(":disabled")
+
+Scenario: Filtering by Room x Period
+  When I login in with email "lucas@gmail.com" and password "123456"
+  Then I visit site page "/booking/searchbookingg/"
+  Then I choose "Room x Period"
+  Then There should be exactly 1 elements matching $(":disabled")
--- sas/sas/steps/steps.py
+++ sas/sas/steps/steps.py
@@ -10,57 +10,64 @@ from django.test import Client
 
 @step(r'I type in "(.*)" to "(.*)"')
 def fill_bootstrap_field(step, text, field):
-	words_list = field.lower().split()
-	words_list.insert(0, "id")
-	id_field = "_".join(words_list)
-	date_field = find_any_field(world.browser, TEXT_FIELDS, id_field)
-	date_field.send_keys(text)
+    words_list = field.lower().split()
+    words_list.insert(0, "id")
+    id_field = "_".join(words_list)
+    date_field = find_any_field(world.browser, TEXT_FIELDS, id_field)
+    date_field.send_keys(text)
 
 
 @step(r'I type in "(.*)" to id "(.*)"')
 def fill_bootstrap_field(step, text, id_field):
-	date_field = find_any_field(world.browser, TEXT_FIELDS, id_field)
-	date_field.send_keys(text)
+    date_field = find_any_field(world.browser, TEXT_FIELDS, id_field)
+    date_field.send_keys(text)
 
 
 @step(r'I click on an element with id of "(.*)"')
 def click_on_element_by_id(step, id):
-	try:
-		elem = world.browser.find_element_by_id(id)
-	except NoSuchElementException:
-		raise AssertionError("Element with ID '{}' not found.".format(id))
-	elem.click()
+    try:
+        elem = world.browser.find_element_by_id(id)
+    except NoSuchElementException:
+        raise AssertionError("Element with ID '{}' not found.".format(id))
+    elem.click()
 
 
 @step(r'I click on an element "(.*)" called "(.*)"')
 def click_on_element_by_value(step, value, typeelement):
-	try:
-		text = find_field_by_value(world.browser, typeelement, elementtext)
-	except NoSuchElementException:
-		raise AssertionError("Element not found.")
-	text.click()
+    try:
+        text = find_field_by_value(world.browser, typeelement, elementtext)
+    except NoSuchElementException:
+        raise AssertionError("Element not found.")
+    text.click()
 
 
 @step(r'I register the user "(.*)" with the password "(.*)" and registration number "(.*)"')
 def register_user(step, username, password,registration_number):
-	user = UserProfile()
-	user.user = User()				
-	user.registration_number = registration_number	
-	user.user.email = username
-	user.user.username = username
-	user.user.first_name = "Usuário"
-	user.user.set_password(password)
-	user.save()
+    user = UserProfile()
+    user.user = User()
+    user.registration_number = registration_number
+    user.user.email = username
+    user.user.username = username
+    user.user.first_name = "Usuário"
+    user.user.set_password(password)
+    user.save()
 
 
 @step(r'I login in with email "(.*)" and password "(.*)"')
 def login_user(step, email, password):
-	step.given("I visit site page \"/\"")
-	c = Client()
-	response = c.login(username=email, password=password)
-	cookies = {}
-	for co in c.cookies.values():
-		cookies['name'] = co.key
-		cookies['value'] = co.value
-		world.browser.add_cookie(cookies)
-	world.browser.refresh()
+    step.given("I visit site page \"/\"")
+    c = Client()
+    response = c.login(username=email, password=password)
+    cookies = {}
+    for co in c.cookies.values():
+        cookies['name'] = co.key
+        cookies['value'] = co.value
+        world.browser.add_cookie(cookies)
+    world.browser.refresh()
+
+@step(r'I see the input with name "(.*)" disabled')
+def disabled_element(step, element_id):
+    element = world.browser.find_element_by_id(element_id);
+
+    if (element.isEnabled()):
+        raise AssertionError("Element is not disabled.".format(id))
