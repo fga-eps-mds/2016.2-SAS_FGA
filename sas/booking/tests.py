2489786a4afc486fb9c772cc41b052062748ad2c
Vitor Menezes <menezesvitorg@gmail.com>
2016-10-19 23:37:29 -0200

    Fixed booking tests.
    Signed-off-by: Vitor Menezes <menezesvitorg@gmail.com>

--- sas/booking/tests.py
+++ sas/booking/tests.py
@@ -8,13 +8,15 @@ from booking.factories import BookingFactory, BookTimeFactory
 from django.urls import reverse
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
+from booking.views import delete_booking
+from booking.urls import *
 
 class TestBookTime(TestCase):
 
 	def test_get_str_weekday(self):
 		book = BookTime()
 		book.date_booking = datetime.strptime("21092016", "%d%m%Y")
-		self.assertEqual(book.get_str_weekday(), "Wednesday") 
+		self.assertEqual(book.get_str_weekday(), "Wednesday")
 
 class BookingTest(TestCase):
 
@@ -22,27 +24,18 @@ class BookingTest(TestCase):
         self.user = UserFactory.create()
         self.user.set_password('1234567')
         self.user.save()
-        self.booking = BookingFactory.create()
+        self.booktimes = [BookTimeFactory.create() for x in range(5)]
+        self.booking = BookingFactory.create(booktimes = self.booktimes)
         self.booking.user = self.user
-        self.booking.user.save()
         self.booking.save()
-        booktimes = [BookTimeFactory.create() for x in range(5)]
-        for booktime in booktimes:
-            booktime.booking = self.booking
-            booktime.save()
-        self.booking.save()
-        self.booking = BookingFactory.create(time = booktimes)
         self.client = Client()
-        content_type = ContentType.objects.get_for_model(UserProfile)
-        permission = Permission.objects.create(
-            codename = 'can_delete',
-            name = 'Can delete any booking',
-            content_type = content_type,
-        )
-        #self.user.user.user_permissions.add(permission)
 
     def test_has_permission_delete_booking(self):
         self.client.login(username = self.user.username, password = '1234567')
-        url = reverse('booking:deletebooking', args = (booking.id,))
-        response = self.client.get('booking/deletebooking')
+        url = reverse('booking:deletebooking', args = (self.booking.id,))
+        #print(Booking.objects.get(pk = self.booking.id))
+        response = self.client.get(url)
+        for booktime in self.booktimes:
+            self.assertFalse(BookTime.objects.filter(pk = booktime.id).exists())
+        self.assertFalse(Booking.objects.filter(pk = self.booking.id).exists())
         self.assertContains(response, 'Booking deleted!')
