26443a050b932642f0f3f24c923ee33528996607
Luis Gustavo <luis.gustavo3013@hotmail.com>
2016-10-21 21:02:40 -0200

    Added period_room table unit test
    Signed-off-by: Luis Gustavo <luis.gustavo3013@hotmail.com>
    Signed-off-by: Hugo Carvalho <hugo-nc@hotmail.com>

--- sas/booking/tests.py
+++ sas/booking/tests.py
@@ -13,6 +13,7 @@ from booking.urls import *
 from user.models import UserProfile
 from booking.forms import BookingForm, SearchBookingForm
 from dateutil import parser
+from booking.views import search_booking_room_period
 
 class TestBookTime(TestCase):
     def test_get_str_weekday(self):
@@ -34,21 +35,10 @@ class TestSearchBookingQuery(TestCase):
         form = SearchBookingForm(data=parameters)
         form.is_valid()
         days = []
-        days.append(start_date)
-        days.append(end_date)
+        days.append(start_date.date())
+        days.append(end_date.date())
         days2 = form.days_list()
         self.assertEqual(days,days2)
-   
-    def test_search(self):
-        start_date = parser.parse(2018-10-10)
-        end_date = parser.parse(2018-10-11)
-        building_name = Building.objects.get(name='UAC')
-        room_name = Place.objects.get(pk=8)
-        parameters = {'search_options': 'opt_room_period','building_name': building_name,
-            'room_name' : room_name, 'start_date' : start_date}
-        form = SearchBookingForm(data=parameters)
-        bookings_test = []
-        bookings_test.append(form)
 
 	def test_get_str_weekday(self):
 		book = BookTime()
@@ -269,3 +259,18 @@ class TestSearchBookingQuery(TestCase):
 			'room_name' : room_name, 'start_date' : start_date}
         form = SearchBookingForm(data=parameters)
         self.assertTrue(form.is_valid())
+
+    def test_search_booking_room_period(self):
+        factory = self.factory
+        start_date = datetime.now().date()
+        end_date = start_date + timedelta(days=50)
+        building_name = Building.objects.filter(name='UAC')
+        room_name = Place.objects.filter(pk=8)
+        parameters = {'search_options' : 'opt_room_period', 'building_name' : building_name, 'room_name' : room_name,
+            'start_date' : start_date, 'end_date' : end_date}
+        form = SearchBookingForm(data=parameters)
+        form.is_valid()
+        request = factory.post('/booking/searchbookingg', parameters)
+        page = search_booking_room_period(request=request,form_booking=form)
+        self.assertEqual(page.status_code,200)
+        self.assertContains(page, "Room x Period")
