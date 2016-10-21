23052cccab62cb4f65a228e5f0e430cb6952d9bd
Hugo Carvalho <hugo-nc@hotmail.com>
2016-10-21 17:39:05 -0200

    Created unit test for get_day
    Signed-off-by: Hugo Carvalho <hugo-nc@hotmail.com>
    Signed-off-by: Fabiola <fabiola.mfleury@gmail.com>

--- sas/booking/tests.py
+++ sas/booking/tests.py
@@ -1,34 +1,49 @@
-from django.test import TestCase
+from django.test import TestCase,RequestFactory
 from booking.models import *
 from django.test import Client
 from booking.factories import *
 from datetime import datetime
 from user.factories import UserFactory
-from datetime import datetime, timedelta
+from booking.forms import SearchBookingForm
+from booking.views import search_booking_building_day
 
 class TestBookTime(TestCase):
 
-    def test_get_str_weekday(self):
-        book = BookTime()
-        book.date_booking = datetime.strptime("21092016", "%d%m%Y")
-        self.assertEqual(book.get_str_weekday(), "Wednesday")
-
-class TestSearchBooking(TestCase):
-    def setUp(self):
-        self.client = Client()
-
-    def test_get_request(self):
-        response = self.client.get('/booking/searchbookingg/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'booking/searchBookingQuery.html')
+	def test_get_str_weekday(self):
+		book = BookTime()
+		book.date_booking = datetime.strptime("21092016", "%d%m%Y")
+		self.assertEqual(book.get_str_weekday(), "Wednesday")
 
 
-    def test_search_booking_post_not_valid(self):
-        client = self.client
-        start_date = datetime.now().date() - timedelta(days=6)
-        building_name = Building.objects.filter(name='UAC')
-        room_name = Place.objects.filter(pk=8)
-        parameters = {'search_options': 'opt_day_room','building_name': building_name,
-            'room_name' : room_name, 'start_date' : start_date}
-        response = client.post('/booking/searchbookingg/', parameters)
-        self.assertTemplateUsed(response, 'booking/searchBookingQuery.html')
+class TestSearchBookingForm(TestCase):
+	
+	def setUp(self):
+		self.factory = RequestFactory()
+
+	def test_get_day(self):
+
+		start_date = datetime.strptime("12/31/2016", "%m/%d/%Y")
+		parameters = {'start_date':start_date}
+		booking = SearchBookingForm(data=parameters)
+
+		if booking.is_valid():
+			aux = booking.get_day()
+			self.assertEqual(start_date,day)
+
+	def test_search_booking_building_day(self):
+		factory = self.factory
+		
+		start_date = datetime.now().date()
+		building_name = Building.objects.filter(name='UAC')
+		parameters = {'search_options': 'opt_building_day','building_name': building_name,
+		'start_date' : start_date}
+
+		form = SearchBookingForm(data=parameters)
+		
+		if form.is_valid():
+			request = self.factory.post('/booking/searchbookingg/', parameters)
+			page = search_booking_building_day(request=request,form_booking=form)
+			
+			self.assertTemplateUsed(page, 'booking/template_table.html')
+
+			
