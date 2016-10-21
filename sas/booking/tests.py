7bbe99941a142095c3a3e1d053657367d6911368
fabiolamfleury <fabiola.mfleury@gmail.com>
2016-10-21 20:11:50 -0200

    Added unit test for table
    Signed-off-by: Hugo Neves <hugo-nc@hotmail.com>
    Signed-off-by: fabiolamfleury <fabiola.mfleury@gmail.com>

--- sas/booking/tests.py
+++ sas/booking/tests.py
@@ -1,10 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from booking.models import *
 from django.test import Client
 from booking.factories import *
 from datetime import datetime
 from user.factories import UserFactory
 from booking.forms import SearchBookingForm
+from booking.views import search_booking_day_room
+
 
 class TestBookTime(TestCase):
 
@@ -15,6 +17,8 @@ class TestBookTime(TestCase):
 
 
 class TestSearchBookingQuery(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
     def test_count_days(self):
         form = SearchBookingForm()
         start_date=datetime.strptime("21092016", "%d%m%Y")
@@ -23,8 +27,19 @@ class TestSearchBookingQuery(TestCase):
         days2 = form.count_days(start_date=start_date,end_date=end_date)
         self.assertEqual(days,days2)
 
-
-
+    def test_search_booking_day_room(self):
+        factory = self.factory
+        start_date = datetime.now().date()
+        building_name = Building.objects.filter(name='UAC')
+        room_name = Place.objects.filter(pk=8)
+        parameters = {'search_options': 'opt_day_room','building_name': building_name,
+            'room_name' : room_name, 'start_date' : start_date}
+        form = SearchBookingForm(data=parameters)
+        if form.is_valid():
+            request = factory.post('/booking/searchbookingg', parameters)
+            page = search_booking_day_room(request=request,form_booking=form)
+            self.assertEqual(page.status_code,200)
+            self.assertContains(page, "Room x Day")
     def test_form_is_valid(self):
         start_date = datetime.now().date()
         building_name = Building.objects.filter(name='UAC')
