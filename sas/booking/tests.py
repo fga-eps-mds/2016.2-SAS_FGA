2a93b0d8bcbb375c97c4632c937cd614655b06a8
Pedro Alcantara <ptralcantara@gmail.com>
2016-10-21 16:33:48 -0200

    Adapting booking/tests.py to pep8

--- sas/booking/tests.py
+++ sas/booking/tests.py
@@ -1,13 +1,11 @@
 from django.test import TestCase
-from booking.models import *
-from django.test import Client
-from booking.factories import *
+from booking.models import BookTime
 from datetime import datetime
-from user.factories import UserFactory
+
 
 class TestBookTime(TestCase):
 
-	def test_get_str_weekday(self):
-		book = BookTime()
-		book.date_booking = datetime.strptime("21092016", "%d%m%Y")
-		self.assertEqual(book.get_str_weekday(), "Wednesday")
+    def test_get_str_weekday(self):
+        book = BookTime()
+        book.date_booking = datetime.strptime("21092016", "%d%m%Y")
+        self.assertEqual(book.get_str_weekday(), "Wednesday")
