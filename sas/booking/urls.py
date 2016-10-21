f1a989b76f443c9c21451e9172a0d0e4b0480bd4
Pedro Alcantara <ptralcantara@gmail.com>
2016-10-21 14:21:15 -0200

    Adapting booking/url.py to pep8

--- sas/booking/urls.py
+++ sas/booking/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
-from django.contrib import admin
-from .views import new_booking, search_booking, search_booking_query, search_booking_table
+from .views import new_booking, search_booking, search_booking_query
 from .views import confirm_booking, cancel_booking
 
 urlpatterns = [
@@ -8,7 +7,8 @@ urlpatterns = [
     url(r'^searchbooking/$', search_booking, name='searchbooking'),
     url(r'^confirmbooking/(\d+)$', confirm_booking, name='confirmbooking'),
     url(r'^cancelbooking/(\d+)$', cancel_booking, name='cancelbooking'),
-	url(r'^searchbookingquery/$', search_booking_query, name='searchbookingquery'),
-	url(r'^searchbookingg/$', search_booking_query, name='searchbookingtable'),
-
+    url(r'^searchbookingquery/$',
+        search_booking_query,
+        name='searchbookingquery'),
+    url(r'^searchbookingg/$', search_booking_query, name='searchbookingtable'),
 ]
