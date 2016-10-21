836ed1fad3cd21bdafcee074edb4a332e94c209d
Pedro Alcantara <ptralcantara@gmail.com>
2016-10-21 14:50:40 -0200

    Adapting booking/factories.py to pep8

--- sas/booking/factories.py
+++ sas/booking/factories.py
@@ -1,19 +1,18 @@
 from factory.django import DjangoModelFactory
-from booking.models import *
+from booking.models import Place
 from faker import Factory as FakerFactory
 from factory import *
 import factory
-from factory.fuzzy import FuzzyChoice
 
 fake = FakerFactory.create()
 
 
 class PlaceFactory(DjangoModelFactory):
 
-	class Meta:
-		model = Place
-		django_get_or_create = ('name', 'capacity', 'is_laboratory')
+    class Meta:
+        model = Place
+        django_get_or_create = ('name', 'capacity', 'is_laboratory')
 
-	name = factory.Sequence(lambda x: 'I%s' % x)
-	capacity = factory.Sequence(lambda x: '%s' % x)
-	is_laboratory = False
+    name = factory.Sequence(lambda x: 'I%s' % x)
+    capacity = factory.Sequence(lambda x: '%s' % x)
+    is_laboratory = False
