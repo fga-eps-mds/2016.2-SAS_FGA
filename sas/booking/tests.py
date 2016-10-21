52da94e482a76a454917d8e0bd088bb78161faa0
Luis Gustavo <luis.gustavo3013@hotmail.com>
2016-10-21 21:02:40 -0200

    Added period_room table unit test
    Signed-off-by: Luis Gustavo <luis.gustavo3013@hotmail.com>
    Signed-off-by: Hugo Carvalho <hugo-nc@hotmail.com>

--- sas/booking/tests.py
+++ sas/booking/tests.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 from user.factories import UserFactory
 from booking.forms import SearchBookingForm
 from dateutil import parser
+from booking.views import search_booking_room_period
 
 class TestBookTime(TestCase):
     def test_get_str_weekday(self):
@@ -27,16 +28,23 @@ class TestSearchBookingQuery(TestCase):
         form = SearchBookingForm(data=parameters)
         form.is_valid()
         days = []
-        days.append(start_date)
-        days.append(end_date)
+        days.append(start_date.date())
+        days.append(end_date.date())
         days2 = form.days_list()
         self.assertEqual(days,days2)
    
-    '''def test_search(self):
-        start_date = parser.parse(2018-10-10)
-        end_date = parser.parse(2018-10-11)
-        building_name = Building.objects.get(name='UAC')
-        room_name = Place.objects.get(pk=8)
-        parameters = {'search_options': 'opt_room_period','building_name': building_name,
-            'room_name' : room_name, 'start_date' : start_date}
-        form = SearchBookingForm(data=parameters)'''
+    def test_search_booking_room_period(self):
+        factory = self.factory
+        start_date = datetime.now().date()
+        end_date = start_date + timedelta(days=50)
+        building_name = Building.objects.filter(name='UAC')
+        room_name = Place.objects.filter(pk=8)
+        parameters = {'search_options' : 'opt_room_period', 'building_name' : building_name, 'room_name' : room_name,
+            'start_date' : start_date, 'end_date' : end_date}
+        form = SearchBookingForm(data=parameters)
+        form.is_valid()
+        request = factory.post('/booking/searchbookingg', parameters)
+        page = search_booking_room_period(request=request,form_booking=form)
+        self.assertEqual(page.status_code,200)
+        self.assertContains(page, "Room x Period")
+
