82957bf091a13802cb4dbd1f2cd62c2634f272fe
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2016-10-22 13:04:16 -0200

    Fixing identation on booking/urls
    Signed-off-by: Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
    Signed-off-by: Pedro Alcantara <ptralcantara@gmail.com>

--- sas/booking/urls.py
+++ sas/booking/urls.py
@@ -1,17 +1,30 @@
 from django.conf.urls import url
 from django.contrib import admin
-from .views import new_booking, search_booking, search_booking_query, search_booking_table
-from .views import confirm_booking, cancel_booking, delete_booking, delete_booktime
+from .views import new_booking
+from .views import search_booking
+from .views import search_booking_query
+from .views import search_booking_table
+from .views import confirm_booking
+from .views import cancel_booking
+from .views import delete_booking
+from .views import delete_booktime
 
 urlpatterns = [
-    url(r'^newbooking/$', new_booking, name='newbooking'),
-    url(r'^searchbooking/$', search_booking, name='searchbooking'),
-    url(r'^confirmbooking/(\d+)$', confirm_booking, name='confirmbooking'),
-    url(r'^cancelbooking/(\d+)$',cancel_booking, name='cancelbooking'),
+    url(r'^newbooking/$',
+        new_booking, name='newbooking'),
+    url(r'^searchbooking/$',
+        search_booking, name='searchbooking'),
+    url(r'^confirmbooking/(\d+)$',
+        confirm_booking, name='confirmbooking'),
+    url(r'^cancelbooking/(\d+)$',
+        cancel_booking, name='cancelbooking'),
     url(r'^searchbookingquery/$',
         search_booking_query,
         name='searchbookingquery'),
-    url(r'^searchbookingg/$', search_booking_query, name='searchbookingtable'),
-    url(r'^deletebooking/(\d+)$', delete_booking, name='deletebooking'),
-    url(r'^deletebooktime/(\d+)/(\d+)$', delete_booktime, name='deletebooktime')
+    url(r'^searchbookingg/$',
+        search_booking_query, name='searchbookingtable'),
+    url(r'^deletebooking/(\d+)$',
+        delete_booking, name='deletebooking'),
+    url(r'^deletebooktime/(\d+)/(\d+)$',
+        delete_booktime, name='deletebooktime')
 ]
