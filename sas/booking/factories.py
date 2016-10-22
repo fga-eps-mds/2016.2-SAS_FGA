584c9b83b8b870ec9f2cbf073280e93a5c765147
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2016-10-22 11:49:24 -0200

    Fixing identation issues on booking/factories
    Signed-off-by: Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
    Signed-off-by: Pedro Alcantara <ptralcantara@gmail.com>

--- sas/booking/factories.py
+++ sas/booking/factories.py
@@ -22,20 +22,21 @@ class PlaceFactory(DjangoModelFactory):
     capacity = factory.Sequence(lambda x: '%s' % x)
     is_laboratory = False
 
+
 class BookingFactory(DjangoModelFactory):
 
     class Meta:
         model = Booking
-        django_get_or_create = ('user', 'place', 'name', 'start_date', 'end_date')
+        django_get_or_create = ('user', 'place', 'name',
+                                'start_date', 'end_date')
 
     user = factory.SubFactory(UserFactory)
-    call_command('loaddata', 'booking/fixtures/buildings.json', verbosity = 0)
-    call_command('loaddata', 'booking/fixtures/places.json', verbosity = 0)
+    call_command('loaddata', 'booking/fixtures/buildings.json', verbosity=0)
+    call_command('loaddata', 'booking/fixtures/places.json', verbosity=0)
     place = FuzzyChoice(Place.objects.all())
     name = factory.LazyAttribute(lambda x: fake.name())
     start_date = FuzzyDate(datetime(2017, 1, 1), datetime(2017, 5, 31))
     end_date = FuzzyDate(datetime(2017, 6, 1), datetime(2017, 12, 31))
-    #end_date = factory.LazyAttribute(lambda o : FuzzyDate(datetime(year = o.start_date.year, month = o.start_date.month, day = o.start_date.day), datetime(2017, 12, 31)))
 
     @factory.post_generation
     def booktimes(self, create, extracted, **kwargs):
@@ -45,12 +46,14 @@ class BookingFactory(DjangoModelFactory):
             for booktime in extracted:
                 self.time.add(booktime)
 
+
 class BookTimeFactory(DjangoModelFactory):
 
     class Meta:
         model = BookTime
         django_get_or_create = {'start_hour', 'end_hour', 'date_booking'}
 
-    start_hour = time(hour = randint(0, 23), minute = randrange(0, 50, 10))
-    end_hour = time(hour = randint(int(start_hour.hour), 23), minute = randrange(0, 50, 10))
+    start_hour = time(hour=randint(0, 23), minute=randrange(0, 50, 10))
+    end_hour = time(hour=randint(int(start_hour.hour), 23),
+                    minute=randrange(0, 50, 10))
     date_booking = FuzzyDate(datetime(2017, 1, 1), datetime(2017, 12, 31))
