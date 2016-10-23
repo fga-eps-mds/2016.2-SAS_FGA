ea4d3f102a71a0fc16a7a16fff37a417a4cf41a9
Vitor Menezes <menezesvitorg@gmail.com>
2016-10-23 00:17:18 -0200

    Created make user an admin test.
    Signed-off-by: Vitor Menezes <menezesvitorg@gmail.com>
    Signed-off-by: lucaaas <lucasoliveira2306@gmail.com>

--- sas/user/tests.py
+++ sas/user/tests.py
@@ -4,6 +4,7 @@ from django.test import Client
 from user.views import edit_user
 from user.factories import UserProfileFactory
 from django.contrib.auth import logout
+from django.urls import reverse
 
 
 class EditUserTest(TestCase):
@@ -256,3 +257,23 @@ class LogoutTest(TestCase):
         self.assertEqual(response.redirect_chain, [('/', 302)])
         self.assertNotContains(response,
                                'You have been logged out sucessfully!')
+
+class MakeUserAnAdminTest(TestCase):
+
+    def setUp(self):
+        self.admin = UserProfileFactory.create()
+        self.admin.user.set_password('1234567')
+        self.admin.make_as_admin()
+        self.admin.save()
+        self.user = UserProfileFactory.create()
+        self.client = Client()
+
+    def test_make_academic_staff_an_admin(self):
+        self.client.login(username=self.admin.user.username, password='1234567')
+        self.user.make_as_academic_staff()
+        url = reverse('user:usertoadmin', args = (self.user.id,))
+        response = self.client.get(url)
+        print(response.content)
+        self.assertContains(response, 'User ' + self.user.full_name() + ' is now an admin.')
+        self.assertFalse(self.user.is_academic_staff())
+        self.assertTrue(self.user.is_admin())
