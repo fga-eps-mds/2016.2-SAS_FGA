b657896d7904a8234cb94b5f3d4292c38c95f0d5
Vitor Menezes <menezesvitorg@gmail.com>
2016-10-23 00:18:39 -0200

    Create make an user an admin view method.
    Signed-off-by: Vitor Menezes <menezesvitorg@gmail.com>
    Signed-off-by: Luis Gustavo <luis.gustavo3013@hotmail.com>'

--- sas/user/views.py
+++ sas/user/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 from .models import UserProfile
 from django.contrib import messages
 from sas.views import index
+from django.contrib.auth.decorators import login_required
 
 
 def new_user(request):
@@ -98,3 +99,21 @@ def change_password(request):
         return index(request)
     else:
         return render_edit_user(request)
+
+@login_required(login_url='/?showLoginModal=yes')
+def make_user_an_admin(request, id):
+    if request.user.profile_user.is_admin():
+        try:
+            user = UserProfile.objects.get(pk = id)
+            if user.profile_user.is_academic_staff:
+                user.user.groups.clear()
+                user.make_as_admin()
+                messages.success(request, _('User ' + user.full_name() + ' is now an admin.'))
+            else:
+                messages.error(request, _('User ' + user.full_name() + ' is already an admin.'))
+        except:
+            messages.error(request, _('User not found.'))
+    else:
+        messages.error(request, _('You cannot access this page.'))
+        redirect('index')
+    return render(request, 'user/searchUser.html', {})
