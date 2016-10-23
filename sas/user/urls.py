b8de66f5a469574e59add3b3db85c375eb283387
Vitor Menezes <menezesvitorg@gmail.com>
2016-10-23 00:16:06 -0200

    Created url for making an user an admin.
    Signed-off-by: Vitor Menezes <menezesvitorg@gmail.com>
    Signed-off-by: lucaaas <lucasoliveira2306@gmail.com>

--- sas/user/urls.py
+++ sas/user/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from .views import new_user, login_user, logout_user
 from .views import delete_user, edit_user
 from .views import change_password, search_user
+from .views import make_user_an_admin
 
 urlpatterns = [
     url(r'newuser/', new_user, name='newuser'),
@@ -11,4 +12,5 @@ urlpatterns = [
     url(r'^edituser/$', edit_user, name='edituser'),
     url(r'^change/$', change_password, name='changepassword'),
     url(r'^searchuser/$', search_user, name='searchuser'),
+    url(r'^usertoadmin/(\d+)$', make_user_an_admin, name='usertoadmin'),
 ]
