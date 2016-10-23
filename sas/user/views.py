d89c5df7f30838a41a58feb4be0e3e42e890d3a9
Vitor Menezes <menezesvitorg@gmail.com>
2016-10-23 01:08:16 -0200

    Fixed user views to pep8.
    Signed-off-by: Vitor Menezes <menezesvitorg@gmail.com>
    Signed-off-by: Luis Gustavo <luis.gustavo3013@hotmail.com>

--- sas/user/views.py
+++ sas/user/views.py
@@ -116,13 +116,15 @@ def search_user(request):
 def make_user_an_admin(request, id):
     if request.user.profile_user.is_admin():
         try:
-            user = UserProfile.objects.get(pk = id)
+            user = UserProfile.objects.get(pk=id)
             if user.is_academic_staff():
                 user.user.groups.clear()
                 user.make_as_admin()
-                messages.success(request, _('User ' + user.full_name() + ' is now an admin.'))
+                messages.success(request, _('User ' + user.full_name() +
+                                            ' is now an admin.'))
             else:
-                messages.error(request, _('User ' + user.full_name() + ' is already an admin.'))
+                messages.error(request, _('User ' + user.full_name() +
+                                          ' is already an admin.'))
         except:
             messages.error(request, _('User not found.'))
         finally:
