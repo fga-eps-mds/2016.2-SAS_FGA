176bdec60e4057fa832b43c67976870554e74652
Vitor Menezes <menezesvitorg@gmail.com>
2016-10-24 18:42:17 -0200

    Fixed user template tags to pep8.
    Signed-off-by: Vitor Menezes <menezesvitorg@gmail.com>
    Signed-off-by: Luis Gustavo <luis.gustavo3013@hotmail.com>

--- sas/user/templatetags/user_extras.py
+++ sas/user/templatetags/user_extras.py
@@ -20,7 +20,7 @@ def is_admin(user):
 
 @register.filter(name='is_admin')
 def is_admin(user):
-    if hasattr(user,"profile_user"):
+    if hasattr(user, "profile_user"):
         return user.profile_user.is_admin()
     else:
         return False
