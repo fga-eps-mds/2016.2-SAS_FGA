92b9241a3667cfe0ef5423ad32cc433ba94c04d1
Vitor Menezes <menezesvitorg@gmail.com>
2016-10-24 18:39:08 -0200

    Fixing booking factories to pep8.
    Signed-off-by: Vitor Menezes <menezesvitorg@gmail.com>
    Signed-off-by: Luis Gustavo <luis.gustavo3013@hotmail.com>

--- sas/booking/factories.py
+++ sas/booking/factories.py
@@ -11,6 +11,7 @@ from django.core.management import call_command
 
 fake = FakerFactory.create()
 
+
 class BuildingFactory(DjangoModelFactory):
 
     class Meta:
@@ -19,11 +20,12 @@ class BuildingFactory(DjangoModelFactory):
 
     name = factory.Sequence(lambda x: 'Building %s' % x)
 
+
 class PlaceFactory(DjangoModelFactory):
 
     class Meta:
         model = Place
-        django_get_or_create = ('name', 'capacity', 'is_laboratory', 
+        django_get_or_create = ('name', 'capacity', 'is_laboratory',
                                 'building')
 
     name = factory.Sequence(lambda x: 'I%s' % x)
@@ -31,32 +33,34 @@ class PlaceFactory(DjangoModelFactory):
     is_laboratory = False
     building = factory.SubFactory(BuildingFactory)
 
+
 class BookTimeFactory(DjangoModelFactory):
 
     class Meta:
         model = BookTime
         django_get_or_create = ('start_hour', 'end_hour', 'date_booking')
 
-    start_hour = time(hour = randrange(0, 22, 2), minute = randrange(0, 50, 10))
+    start_hour = time(hour=randrange(0, 22, 2), minute=randrange(0, 50, 10))
     date_booking = date.today()
 
     @factory.lazy_attribute
     def end_hour(self):
-        hour = randrange(self.start_hour.hour,22,2)
-        minute = randrange(0,50,10)
-        return time(hour=hour,minute=minute)
+        hour = randrange(self.start_hour.hour, 22, 2)
+        minute = randrange(0, 50, 10)
+        return time(hour=hour, minute=minute)
+
 
 class BookingFactory(DjangoModelFactory):
-    
+
     class Meta:
         model = Booking
-        django_get_or_create = ('user','place','name','start_date',
+        django_get_or_create = ('user', 'place', 'name', 'start_date',
                                 'end_date')
 
     place = factory.SubFactory(PlaceFactory)
     name = factory.Sequence(lambda x: 'Testando %s' % x)
-    start_date = datetime.now() 
-    end_date = fake.date_time_this_year(before_now=False,after_now=True)
+    start_date = datetime.now()
+    end_date = fake.date_time_this_year(before_now=False, after_now=True)
 
     @factory.lazy_attribute
     def user(self):
@@ -68,10 +72,10 @@ class BookingFactory(DjangoModelFactory):
         if create:
             num_created = 0
             start_date = self.start_date
-            while start_date.date() < self.end_date.date() and start_date.date() != self.end_date.date():
+            while start_date.date() < self.end_date.date() and \
+                    start_date.date() != self.end_date.date():
                 start_date = fake.date_time_between_dates(start_date,
-                                                            self.end_date)
-                book = BookTimeFactory(date_booking = start_date)
+                                                          self.end_date)
+                book = BookTimeFactory(date_booking=start_date)
                 book.save()
                 self.time.add(book)
-
