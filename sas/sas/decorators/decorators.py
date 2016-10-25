fdaa85c1293dadebc26489a069b25531f20e3b3b
Vitor Menezes <menezesvitorg@gmail.com>
2016-10-24 22:35:37 -0200

    Created required to be admin decorator.
    Signed-off-by: Vitor Menezes <menezesvitorg@gmail.com>

---
+++ sas/sas/decorators/decorators.py
@@ -0,0 +1,14 @@
+from sas.views import index
+from functools import wraps
+from django.contrib import messages
+
+def required_to_be_admin(function):
+    def wrap(request, *args, **kwargs):
+        if hasattr(request.user, 'profile_user') and request.user.profile_user.is_admin():
+            return function(request, *args, **kwargs)
+        else:
+            messages.error(request, "You cannot access this page.")
+            return index(request)
+    wrap.__doc__ = function.__doc__
+    wrap.__name__ = function.__name__
+    return wrap
