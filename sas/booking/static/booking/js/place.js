8f86d845eac4f71afc22340d7eb2bcb2c4a6d9d3
Gustavo Coelho <gutorc@hotmail.com>
2016-10-27 00:48:06 -0200

    Fill in the places page
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>
    Signed-off-by: Jessica Suzuki <suzuki_jessica@hotmail.com>

--- sas/booking/static/booking/js/place.js
+++ sas/booking/static/booking/js/place.js
@@ -1,26 +1,31 @@
 function Place(pk, name){
     this.pk = pk;
     this.name = name;
+    
 
-    this.tran = function(data){
-        all = new Array()
-        for(var i = 0; i < data.length; i++){
-            p = new Place(data[i].pk, data[i].name);
-            all.push(all);
-            console.log(p.pk);
-            console.log(p.name);
-        }
-        return all;
+    this.td_place = function(){
+        var begin = "<td class='place-span'>";
+        var input = "<input type='hidden' value='" + this.pk + "' />";
+         
+        var end = "</td>"
+        return begin + input + this.name + end;
     }
 
     this.all = function(){
-        $.getJSON("/places",  this.tran)
+        all = new Array()
+        $.getJSON("/places", function(data){
+            for(var i = 0; i < data.length; i++){
+                p = new Place(data[i].pk, data[i].name);
+                all.push(p);
+            }
+        });
+        return all;
     }
 }
 
 Place.all = function(){
     p = new Place();
-    p.all()
+    return p.all()
 
 }
 
