50c43d65ad62b06787c142fea65a1623110d6a8d
Gustavo Coelho <gutorc@hotmail.com>
2016-10-29 02:55:06 -0200

    Time and post request
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- sas/booking/static/booking/js/newbooking.js
+++ sas/booking/static/booking/js/newbooking.js
@@ -1,5 +1,34 @@
 var buildings = Building.all();
 
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+var csrftoken = getCookie('csrftoken');
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
 function breadcrumbsadd(index){
     $("#breadcrumbs ul li:eq(" + index + ")").addClass("active")
 
@@ -24,14 +53,49 @@ function test(places){
             }
         }
 }
+$("#slider_begin_time").slider({
+    min: 8, 
+    max: 22, 
+    step: 2,
+    create: function( event, ui ) {
+        $("#input_slider_begin_time").val("8:00");
+    }
+});   
+$("#slider_end_time").slider({
+    min: 8, 
+    max: 22, 
+    step: 2,
+    create: function( event, ui ) {
+        $("#input_slider_end_time").val("8:00");
+    }
+});
 $(document).ready(function(){
     $("#page1").show();
     $("#page2").hide();
     $("#page3").hide();
     $("#page4").hide();
     $("#page5").hide();
-    
-    
+    $("#period-dates").hide();
+  
+    $( "#slider_begin_time" ).on( "slidechange", function( event, ui ) {
+        var text = $( "#slider_begin_time" ).slider("value");
+        text = text + ":00"; 
+        $("#input_slider_begin_time").val(text);
+    });
+    $( "#slider_end_time" ).on( "slidechange", function( event, ui ) {
+        var text = $( "#slider_end_time" ).slider("value");
+        text = text + ":00"; 
+        $("#input_slider_end_time").val(text);
+    });
+    $('input[name=times]', '#page2').click(function(){
+        if($('input[name=times]:checked', '#page2').val() == "interval"){
+           $("#period-dates").show();
+           $("#one-day").hide();
+        }else{
+           $("#period-dates").hide();
+           $("#one-day").show();
+        }
+    });    
     
     $("#next-date").click(function(){
         $("#page1").hide();
@@ -84,6 +148,32 @@ $(document).ready(function(){
         $("#page4").hide();
         $("#page5").show();
         breadcrumbsadd(3);
+        
+        var building = $(".building-selected > input").attr("value");
+        var place = $(".place-selected > input").attr("value");
+        var booking_name = $("#booking_name").val();
+        var start_date = $("#id_start_date").val();
+        var end_date = $("#id_end_date").val();
+        var start_hour = $( "#slider_begin_time" ).slider("value") + ":00:00";
+        var end_hour = $( "#slider_end_time" ).slider("value") + ":00:00";
+        var ar_week_days = Array();
+        $("input[name=week_days]:checked").each(function(index){
+            ar_week_days.push($(this).val());
+        });
+        $.post("/booking/newbooking/",
+               {
+                building     : building,    
+                place        : place,       
+                booking_name : booking_name, 
+                start_date   : start_date,  
+                end_date     : end_date,    
+                start_hour   : start_hour,  
+                end_hour     : end_hour,    
+                week_days    : ar_week_days   
+               }).done(function(result){
+                    console.log(result);
+                    $("#page5").append(result);
+               });      
     });
 });
 
