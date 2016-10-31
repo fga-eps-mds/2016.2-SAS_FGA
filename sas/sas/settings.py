6b55e3df3f6205efd960608d837f90c104a15f87
Gustavo Coelho <gutorc@hotmail.com>
2016-10-31 20:09:47 -0200

    Change for postgres
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- sas/sas/settings.py
+++ sas/sas/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'sas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sas',
+        'USER': 'postgres',
+        'PASSWORD': 'sas123' ,
+        'HOST': '172.17.0.3',
+        'PORT': '5432',
     }
 }
 
