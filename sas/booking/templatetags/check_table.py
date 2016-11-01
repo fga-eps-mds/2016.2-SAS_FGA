ca0444f2a522597c6c08e60a06e00bd2d5e3f455
Hugo Carvalho <hugo-nc@hotmail.com>
2016-11-01 15:16:05 -0200

    Modiefied method for check table
    Signed-off-by: Luis Gustavo <luis.gustavo3013@hotmail.com>
    Signed-off-by: Hugo Carbalho <hugo-nc@hotmail.com>

--- sas/booking/templatetags/check_table.py
+++ sas/booking/templatetags/check_table.py
@@ -2,21 +2,6 @@ from django import template
 
 register = template.Library()
 
-
-@register.assignment_tag
-def check_table(cell_table, id):
-    aux = 0
-
-    for idx_hour, book in cell_table:
-        if idx_hour == id:
-            aux = 1
-            booking = book
-    if aux == 1:
-        return booking.name
-    else:
-        return ''
-
-
 @register.filter(name='check_tooltip')
 def check_tooltip(cell_table, id):
     aux = 0
@@ -27,5 +12,18 @@ def check_tooltip(cell_table, id):
             booking = book
     if aux == 1:
         return booking.user.get_full_name()
+    else:
+        return None
+
+@register.filter(name='check_table')
+def check_table(cell_table, id):
+    aux = 0
+    for idx_hour, book in cell_table:
+        if idx_hour == id:
+            aux = 1
+            booking = book
+
+    if aux == 1:
+   	    return booking.name
     else:
         return None
