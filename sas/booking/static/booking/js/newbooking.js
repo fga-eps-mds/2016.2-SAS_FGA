fb94abf519e47a77cb82820ab094c532d4f96522
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2016-11-03 21:10:56 -0200

    Disabling time inputs
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>
    Signed-off-by: Elaine Cristina Meirelles <ecmeirelles@hotmail.com>

--- sas/booking/static/booking/js/newbooking.js
+++ sas/booking/static/booking/js/newbooking.js
@@ -1,3 +1,35 @@
+function Building(pk, name){
+    this.pk = pk;
+    this.name = name;
+
+
+    this.td_place = function(){
+        var begin = "<td class='place-span'><i class='glyphicon glyphicon-home'></i>&nbsp;";
+        var input = "<input type='hidden' value='" + this.pk + "' />";
+
+        var end = "</td>"
+        return begin + input + this.name + end;
+    }
+
+    this.all = function(){
+        var all = new Array()
+        $.getJSON("/buildings", function(data){
+            for(var i = 0; i < data.length; i++){
+                var p = new Building(data[i].pk, data[i].name);
+                all.push(p);
+            }
+        });
+        return all;
+    }
+
+}
+
+Building.all = function(){
+    var b = new Building();
+    return b.all();
+
+}
+
 var buildings = Building.all();
 
 
@@ -96,15 +128,19 @@ $(document).ready(function(){
 			language: '{% trans "en" %}',
 			autoclose: true,
 	});
+     $("#input_slider_begin_time").attr("disabled", true);
     $( "#slider_begin_time" ).on( "slidechange", function( event, ui ) {
         var text = $( "#slider_begin_time" ).slider("value");
         text = text + ":00";
         $("#input_slider_begin_time").val(text);
     });
+
+    $("#input_slider_end_time").attr("disabled", true);
     $( "#slider_end_time" ).on( "slidechange", function( event, ui ) {
         var text = $( "#slider_end_time" ).slider("value");
         text = text + ":00";
         $("#input_slider_end_time").val(text);
+
     });
     $('input[name=times]', '#page2').click(function(){
         if($('input[name=times]:checked', '#page2').val() == "interval"){
