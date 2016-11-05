8f6c1dd20a8b0231fc47a82be54208c1b6dba597
Hugo Carvalho <hugo-nc@hotmail.com>
2016-11-04 23:04:33 -0200

    Fixing pep8
    Signed-off-by: Hugo Carvalho <hugo-nc@hotmail.com>
    Signed-off-by: Luis Gustavo <luis.gustavo3013@hotmail.com>

--- sas/booking/templatetags/check_table.py
+++ sas/booking/templatetags/check_table.py
@@ -1,7 +1,8 @@
 from django import template
-from datetime import timedelta,datetime
+from datetime import timedelta, datetime
 
 register = template.Library()
+
 
 @register.filter(name='check_tooltip')
 def check_tooltip(cell_table, id):
@@ -16,6 +17,7 @@ def check_tooltip(cell_table, id):
     else:
         return None
 
+
 @register.filter(name='check_table')
 def check_table(cell_table, id):
     aux = 0
@@ -25,52 +27,55 @@ def check_table(cell_table, id):
             booking = book
 
     if aux == 1:
-   	    return booking.name
+        return booking.name
     else:
         return None
 
+
 @register.filter(name='aux_search_date')
-def aux_search_date(days,n):
-    return days,n
+def aux_search_date(days, n):
+    return days, n
+
 
 @register.filter(name='search_date')
-def search_date(daysn,count):
+def search_date(daysn, count):
     days, n = daysn
     day = []
 
     if type(days) is list:
-        if count < n:        
+        if count < n:
             return str(days[count])
     else:
-        return str(days)        
+        return str(days)
 
 
 @register.filter(name='search_hour')
-def search_hour(id,option):
-    
+def search_hour(id, option):
+
     if option == 0:
         time = timedelta(hours=id)
     else:
         if id == 22:
-            time = timedelta(hours=id+2) - timedelta(days=1)
+            time = timedelta(hours=0)
         else:
-            time = timedelta(hours=id+2)        
+            time = timedelta(hours=id + 2)
 
     return time
 
+
 @register.filter(name='search_place')
-def search_place(place,count):
-    
+def search_place(place, count):
+
     if str(type(place)) == "<class 'django.db.models.query.QuerySet'>":
         return str(place[count].pk)
     else:
-        return str(place.pk)        
+        return str(place.pk)
 
 
 @register.filter(name='search_building')
-def search_building(place,count):
-    
+def search_building(place, count):
+
     if str(type(place)) == "<class 'django.db.models.query.QuerySet'>":
         return str(place[count].building.pk)
     else:
-        return str(place.building.pk)        
+        return str(place.building.pk)
