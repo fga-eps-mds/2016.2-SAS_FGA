a777ac0044e48a49c1a155dbd1e617e8eee95de3
Luis Gustavo <luis.gustavo3013@hotmail.com>
2016-11-09 15:37:48 -0200

    Created faq view method
    Signed-off-by: Luis Gustavo <luis.gustavo3013@hotmail.com>
    Signed-off-by: Elaine Cristina Meirelles <ecmeirelles@hotmail.com>

--- sas/sas/views.py
+++ sas/sas/views.py
@@ -9,3 +9,6 @@ def index(request, login_form = LoginForm()):
         return render(request, 'sas/home.html', {})
     else:
         return render(request, 'sas/index.html', {'form' : login_form})
+
+def faq(request):
+    return render(request, 'sas/faq.html')
