660eb309f90a1391c1ad23c12346e24f7f1f4483
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2016-11-10 22:10:06 -0200

    Starting pep8 check
    Signed-off-by: Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
    Signed-off-by: Luis Gustavo <luis.gustavo3013@hotmail.com>

--- sas/sas/urls.py
+++ sas/sas/urls.py
@@ -4,9 +4,9 @@ from .views import index, faq
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index, name = "index"),
+    url(r'^$', index, name="index"),
     url(r'^user/', include('user.urls', namespace="user")),
     url(r'^booking/', include('booking.urls', namespace="booking")),
     url(r'^i18n/', include('django.conf.urls.i18n')),
-    url(r'^faq/', faq, name = "faq"),
+    url(r'^faq/', faq, name="faq"),
 ]
--- sas/sas/views.py
+++ sas/sas/views.py
@@ -1,14 +1,13 @@
-from django.utils.translation import ugettext_lazy as _
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib import messages
+from django.shortcuts import render
 from user.forms import LoginForm
 
 
-def index(request, login_form = LoginForm()):
+def index(request, login_form=LoginForm()):
     if hasattr(request, 'user') and request.user.is_authenticated():
         return render(request, 'sas/home.html', {})
     else:
-        return render(request, 'sas/index.html', {'form' : login_form})
+        return render(request, 'sas/index.html', {'form': login_form})
+
 
 def faq(request):
     return render(request, 'sas/faq.html')
