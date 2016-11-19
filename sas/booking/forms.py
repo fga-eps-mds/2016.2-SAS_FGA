6f57bbf55be0d4b375a28bd009140549d5d8f055
fabiolamfleury <fabiola.mfleury@gmail.com>
2016-11-19 00:59:12 -0200

    Corrected blank tag field problem
    Signed-off-by: Vitor Gomes <menezesvitorg@gmail.com>
    Signed-off-by: fabiolamfleury <fabiola.mfleury@gmail.com>

--- sas/booking/forms.py
+++ sas/booking/forms.py
@@ -270,13 +270,16 @@ class BookingForm(forms.Form):
                         newBookTime.save()
                         booking.time.add(newBookTime)
                 tags = self.cleaned_data['tags']
-                tags = ast.literal_eval(tags)
-                for name in tags:
-                    if not Tag.objects.filter(name=name).exists():
-                        tag = Tag(name=name)
-                        tag.save()
-                    tag =  Tag.objects.get(name=name)
-                    booking.tags.add(tag)
+                tags = re.search('[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+',
+                                  tags)
+                if tags is not None:
+                    tags = ast.literal_eval(tags)
+                    for name in tags:
+                        if not Tag.objects.filter(name=name).exists():
+                            tag = Tag(name=name)
+                            tag.save()
+                            tag =  Tag.objects.get(name=name)
+                            booking.tags.add(tag)
                 booking.save()
 
 
