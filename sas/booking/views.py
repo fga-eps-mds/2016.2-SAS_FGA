05ffad72073e8d6f08a2929a39a979740ca2f7c5
fabiolamfleury <fabiola.mfleury@gmail.com>
2016-11-24 11:43:53 -0200

    Updated tagged bookings
    Signed-off-by: Hugo Carvalho <hugo-nc@hotmail.com>
    Signed-off-by: Fabiola Malta <fabiola.mfleury@gmail.com>

--- sas/booking/views.py
+++ sas/booking/views.py
@@ -404,6 +404,8 @@ def booking_details(request, booking_id):
 
 @login_required(login_url='/?showLoginModal=yes')
 def tagged_bookings(request, tag_id):
+    tag = Booking.objects.get(tags__id=tag_id).name
+    name = _("Tagged Bookings ")
     bookings = Booking.objects.filter(tags__id=tag_id).prefetch_related('tags')
     return render(request, 'booking/searchBooking.html',
-                  {'bookings': bookings})
+                  {'bookings': bookings, 'name': name})
