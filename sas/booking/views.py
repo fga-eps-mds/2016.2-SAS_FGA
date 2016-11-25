dc3f7f93eb7df846cc3461234dc5af04fd7d78a0
Vitor Menezes <menezesvitorg@gmail.com>
2016-11-25 17:14:38 -0200

    Fixed booking already exists message.
    Signed-off-by: Vitor Menezes <menezesvitorg@gmail.com>
    Signed-off-by: Luis Gustavo <luis.gustavo3013@hotmail.com>

--- sas/booking/views.py
+++ sas/booking/views.py
@@ -239,7 +239,7 @@ def new_booking(request):
                 return render(request, 'booking/showDates.html',
                               {'booking': booking})
             else:
-                messages.error(request, _("Booking alread exists"))
+                messages.error(request, _("Booking already exists"))
     else:
         form_booking = BookingForm()
     return render(request, 'booking/newBooking.html',
